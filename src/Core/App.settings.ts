054635db9539822fb8146ddbd57fc2ace1974f69
Bruno V. Vergatti <bruno_vergatti@yahoo.com.br>
2022-11-13 17:01:23 +0200

    Changes index roots

--- src/Core/App.settings.ts
+++ src/Core/App.settings.ts
@@ -1,2 +1,4 @@
 export const APP_TITLE = 'whee';
 export const APP_SLOGAN = 'The most definitive shape store in the world';
+export const APP_MODAL_ROOT = 'modal-root';
+export const APP_BACKDROP_ROOT = 'backdrop-root';
